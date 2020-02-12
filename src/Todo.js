f601854c91fc6fc0e4f58c514eb6495f8f0800d8
github username <therealgracechoi@gmail.com>
2020-02-11 16:54:30 -0800

    toggle to edit responds accordingly

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UseToggleState from './hooks/UseToggleState';
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -9,8 +10,11 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 
 // or add 'props' as argument...
 function Todo({task, id, completed, removeTodo, toggleTodo}) {
+    const [isEditing, toggle] = UseToggleState(false);  
     return(
         <ListItem>
+            {isEditing ? <h1>EDITING TIME!</h1> :
+            <>
             <Checkbox 
                 checked={completed} 
                 tabIndex={-1}
@@ -25,10 +29,12 @@ function Todo({task, id, completed, removeTodo, toggleTodo}) {
                 onClick={() => removeTodo(id)}>
                     <DeleteIcon />
                 </IconButton>
-                <IconButton aria-label="Edit">
+                <IconButton aria-label="Edit" onClick={toggle}>
                     <EditIcon />
                 </IconButton>
             </ListItemSecondaryAction>
+            </>
+            }
         </ListItem>
     )
 }
